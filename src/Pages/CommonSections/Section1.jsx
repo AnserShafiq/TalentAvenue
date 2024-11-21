15cda0bfae50e7df3d5d0de7311a85c985f8a5d4
AnserShafiq <ansershafiq@gmail.com>
2024-11-21 20:11:40 +0500

    Home Section 1

--- src/Pages/CommonSections/Section1.jsx
+++ src/Pages/CommonSections/Section1.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Section1 = ({ background, headline, secondline }) => {
   return (
-    <div className="flex flex-col items-center max-h-[65%] relative">
+    <div className="flex flex-col items-center relative">
       <img 
         src={background} 
-        className="object-cover w-full max-h-[95vh] object-top" 
+        className="object-cover z-0 w-full max-h-[88vh] 2xl:max-h-screen-[88vh] object-top" 
         alt="Talent Avenue - Home 1" 
       />
-      <div className="flex flex-col items-center absolute w-[85%] max-w-[1350px] px-[5%] py-[2%] bottom-[5%] bg-[#303030AD] rounded-[20px]">
+      <div className="flex flex-col items-center absolute w-[85%] max-w-[1350px] px-[5%] py-[2%] bottom-[7%] bg-[#303030AD] rounded-[20px]">
         {headline && (
           <h3 className="text-w-1 text-[45px] leading-tight capitalize font-semibold">
             {headline}
