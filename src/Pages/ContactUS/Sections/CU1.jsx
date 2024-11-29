b953d325fb7bfa9074977f78ae6c5db933b05d47
AnserShafiq <ansershafiq@gmail.com>
2024-11-30 01:06:52 +0500

    Developing contact us

--- src/Pages/ContactUS/Sections/CU1.jsx
+++ src/Pages/ContactUS/Sections/CU1.jsx
@@ -17,28 +17,28 @@ const CU1 = () => {
         <MainHead title={HeadingContent.title} titleColor={'w-1'} description={HeadingContent.desc} descriptionColor={'w-1'}/>
         <div className='flex flex-col lg:flex-row items-center justify-around mt-5'>
             
-            <div className='flex flex-col w-[300px] lg:w-[370px] items-center my-4 lg:my-0 py-4 lg:py-8 px-8 rounded-[20px] bg-transparent-black-70'>
+            <div className='flex flex-col w-[300px] lg:w-[370px] lg:min-h-[280px] items-center my-4 lg:my-0 py-4 lg:py-8 px-8 rounded-[20px] bg-transparent-black-70'>
                 <Mail className='text-[5rem] text-g-1' />
                 <h3 className='text-[1.7rem] lg:text-[2rem] font-semibold uppercase text-g-1 mt-2'>
                     To Mail Us
                 </h3>
-                <Link className='text-[1.2rem] lg:text-[1.5rem] text-w-1 border-b-[1px] border-transparent hover:border-g-1' to={'mailto:info@talentavenue.ca'}>info@talentavenue.ca</Link>
+                <Link className='text-[1.2rem] lg:text-[1.5rem] text-w-1 border-b-[1px] border-transparent transition-all duration-[0.3s] hover:text-g-1 hover:underline hover:scale-[1.04]' to={'mailto:info@talentavenue.ca'}>info@talentavenue.ca</Link>
             </div>
 
-            <div className='flex flex-col w-[300px] lg:w-[370px] items-center my-4 lg:my-0 py-4 lg:py-8  px-8 rounded-[20px] bg-transparent-black-70'>
+            <div className='flex flex-col w-[300px] lg:w-[370px] lg:min-h-[280px] items-center my-4 lg:my-0 py-4 lg:py-8  px-8 rounded-[20px] bg-transparent-black-70'>
                 <Call className='text-[5rem] text-g-1' />
                 <h3 className='text-[1.7rem] lg:text-[2rem] font-semibold uppercase text-g-1 mt-2'>
                     To Contact Us
                 </h3>
-                <Link className='text-[1.2rem] lg:text-[1.5rem] text-w-1 border-b-[1px] border-transparent hover:border-g-1' to={'mailto:info@talentavenue.ca'}>(123) 456 7890</Link>
+                <Link className='text-[1.2rem] lg:text-[1.5rem] text-w-1 border-b-[1px] border-transparent transition-all duration-[0.3s] hover:text-g-1 hover:underline hover:scale-[1.04]' to={'mailto:info@talentavenue.ca'}>(123) 456 7890</Link>
             </div>
 
-            <div className='flex flex-col w-[300px] lg:w-[370px] items-center my-4 lg:my-0 py-4 lg:py-8  px-8 rounded-[20px] bg-transparent-black-70'>
+            <div className='flex flex-col w-[300px] lg:w-[370px] lg:min-h-[280px] items-center my-4 lg:my-0 py-4 lg:py-8  px-8 rounded-[20px] bg-transparent-black-70'>
                 <Office className='text-[5rem] text-g-1' />
                 <h3 className='text-[1.7rem] lg:text-[2rem] font-semibold uppercase text-g-1 mt-2'>
                     Our Location
                 </h3>
-                <Link className='text-[1.2rem] lg:text-[1.5rem] text-w-1 border-b-[1px] border-transparent hover:border-g-1 text-center' to={'mailto:info@talentavenue.ca'}>2424 Finch Ave W#18, Torontto, Canada </Link>
+                <Link className='text-[1.2rem] lg:text-[1.5rem] text-w-1 border-b-[1px] border-transparent transition-all duration-[0.3s] hover:text-g-1 hover:underline hover:scale-[1.04] text-center' to={'mailto:info@talentavenue.ca'}>2424 Finch Ave W#18, Torontto, Canada </Link>
             </div>
 
         </div>
