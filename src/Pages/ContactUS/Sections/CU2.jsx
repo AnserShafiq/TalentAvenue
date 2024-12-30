f1e630007b8842797b35fb7f115c04b73e4e6609
AnserShafiq <ansershafiq@gmail.com>
2024-12-30 22:39:01 +0500

    Added Forms Submission Actions

--- src/Pages/ContactUS/Sections/CU2.jsx
+++ src/Pages/ContactUS/Sections/CU2.jsx
@@ -11,6 +11,7 @@ const CU2 = () => {
         Reason: '',
         Message: '',
     })
+    const [emailSent, setEmailSent] = useState(false);
 
     const handleEntry = (e) => {
       const {name, value} = e.target
@@ -41,23 +42,30 @@ const CU2 = () => {
       });
       if (sendingMail.ok){
           console.log('Contact Form Mail Sent!!!');
+          setEmailSent(true);
       }
     }
-
+    // Thank you for contacting us, our team will reach back to you as soon as possible.
     return (
     <div className="flex flex-col lg:flex-row my-[10%] lg:mt-10 lg:mb-14 w-[95%] lg:w-[80%] 2xl:w-[70%] mx-auto">
       
       <div className="lg:w-[50%] lg:sticky lg:top-[10%]">
         <img
-          className="h-[50vh] lg:h-full w-full object-cover rounded-[20px]"
+          className="h-[50vh] lg:h-full lg:min-h-[72.7vh] w-full object-cover rounded-[20px]"
           src={SidePic}
           alt="Contact Us Form"
         />
       </div>
 
-      <div className="flex flex-col justify-center items-start py-6 lg:py-0 px-6 lg:px-10 lg:w-[50%]">
+      <div className="flex flex-col justify-start items-start py-6 lg:py-0 px-6 lg:px-10 lg:w-[50%]">
         <div className="text-[1.8rem] lg:text-[2.3rem] font-bold mb-4"><Underlining bodytext={'To Leave Us A Message'} textcolor={'w-1'} position={'left'}/></div>
-        <form className='w-full' onSubmit={handleSubmission}>
+        {
+          emailSent? (
+            <div className='flex w-full text-start text-[1.5rem] uppercase font-bold text-g-1'>
+              Thank you for contacting us, our team will reach back to you as soon as possible.
+            </div>
+          ):(
+            <form className='w-full' onSubmit={handleSubmission}>
             <div className='flex flex-col w-full mb-3'>
                 <label className='text-[1.5rem] text-w-1 font-[500] capitalize tracking-wider'>Full name:</label>
                 <input className='w-full px-3 py-2 rounded-[8px] text-[1.1rem] placeholder:text-g-1 lg:text-[1.3rem] text-w-1 bg-[#0000007f]' type='text' name='Name' value={ContactForm.Name} placeholder='Enter your full name' onChange={handleEntry} />
@@ -80,8 +88,8 @@ const CU2 = () => {
             </div>
             <button className='bg-g-1 hover:bg-[#000000af] text-[1.2rem] hover:text-g-1 border-2 border-transparent hover:border-g-1 lg:text-[1.3rem] font-semibold capitalize text-w-1 px-4 py-1 rounded-2xl tracking-wide mt-3' type='submit'>Submit Message</button>
         </form>
-
-
+          )
+        }
       </div>
     </div>
   );
